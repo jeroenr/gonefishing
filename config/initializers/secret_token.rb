68d3bafcf1f9b672995bfbc05ed4ccdccc0da06f
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2012-07-09 21:56:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gonefishing::Application.config.secret_token = 'ac103f7373b292b5128ef3ad824f4585e79a0c333548594970834db2362cf3d56b3140c102f0cdf749527666803ecac5e0c8c1419a07aa56dcf6991e3abe7c61'
